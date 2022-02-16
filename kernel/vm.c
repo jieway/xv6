6632a87f01b651984a54bd04f20cc63a99ca1b3f
weijiew <836678589@qq.com>
2022-02-17 00:19:31 +0800

    PASS Lab3 ex1

--- kernel/vm.c
+++ kernel/vm.c
@@ -440,3 +440,31 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
+void
+vmprint(pagetable_t pagetable)
+{
+  printf("page table %p\n", pagetable);
+  vmprint_helper(pagetable, 1);  
+}
+
+void
+vmprint_helper(pagetable_t pagetable,int hi)
+{
+  // there are 2^9 = 512 PTEs in a page table.
+  for(int i = 0; i < 512; i++){
+    pte_t pte = pagetable[i];
+    if(pte & PTE_V){
+      for (int i = 0; i < hi; i++) {
+        if (i != 0) printf(" ");
+        printf("..");
+      }
+      // this PTE points to a lower-level page table.
+      uint64 child = PTE2PA(pte);
+      printf("%d: pte %p pa %p\n",i,pte,child);
+      if ((pte & (PTE_R|PTE_W|PTE_X)) == 0) {
+        vmprint_helper((pagetable_t)child, hi + 1);
+      }
+    }
+  }
+}
